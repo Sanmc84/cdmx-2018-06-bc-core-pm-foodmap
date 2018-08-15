50a690e9da15a04452aac45e7b21841b6b804115
Sanmc84 <moreloscs200884@gmail.com>
2018-08-15 17:27:51 -0500

    Refactorizando codigo para pintar data

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,24 @@
-let urlData= "https://api.myjson.com/bins/12509k"
-window.onload = (() => {
-  let busqueda = document.getElementById('busquedaPrecio');
-   let taquerias = Object.keys(urlData);
-   console.log(taquerias);
-   
-
-    });
+let urlData = "https://api.myjson.com/bins/12509k"
+ window.onload = (() => {
+//   // let busqueda = document.getElementById('busquedaPrecio');
+//   // let taquerias = Object.value(urlData);
+//   // console.log(taquerias);
+//  
+fetch(urlData)//entro a json para recorrer datos
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(myJson);
+    let datos=Object.values(myJson);
+    
+    for(i = 0; i < datos.length; i++){
+      
+      let rest = datos[i];
+        console.log(rest);
+        console.log(rest.imagen);
+        }
+      
+       
+  });
+});
