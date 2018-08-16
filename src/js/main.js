302ddb18a382dd2200f8c4707fbb5c2a93c06e60
Sanmc84 <moreloscs200884@gmail.com>
2018-08-15 23:15:51 -0500

    Subiendo cambios pinta fotos de la data

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,35 @@
-let urlData = "https://api.myjson.com/bins/12509k"
+let urlData = "https://api.myjson.com/bins/fxwlg"
  window.onload = (() => {
-//   // let busqueda = document.getElementById('busquedaPrecio');
-//   // let taquerias = Object.value(urlData);
-//   // console.log(taquerias);
-//  
+
 fetch(urlData)//entro a json para recorrer datos
   .then(function(response) {
     return response.json();
   })
   .then(function(myJson) {
-    console.log(myJson);
+    // console.log(myJson);
     let datos=Object.values(myJson);
-    
+    let caja= document.getElementById('result');
+    let result = "";
     for(i = 0; i < datos.length; i++){
-      
       let rest = datos[i];
-        console.log(rest);
-        console.log(rest.imagen);
-        }
-      
-       
+      result +=  `   
+        <div class="col-sm-4 numeroFotos">
+          <img  data-toggle="modal" data-img="${rest.imagen}" data-target=".bd-example1-modal-lg" src="${rest.imagen}" alt="">
+      </div>
+    `
+    // console.log(result);
+    caja.innerHTML=result;    
+        }       
   });
 });
+
+let fotos = document.getElementsByClassName("numeroFotos");
+console.log(Array.from(fotos))
+
+
+
+// click.addEventListener("click",event =>{
+//   alert("si funciona")
+// })
+
+
